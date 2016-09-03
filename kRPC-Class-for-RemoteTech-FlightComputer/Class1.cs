cd67b57a15834f81fc553824c8f6d5e42ac36f0e
Eli112358 <Eli112358.SARP@hotmail.com>
2016-09-03 09:12:55 -0400

    VS solution created Initial code

---
+++ kRPC-Class-for-RemoteTech-FlightComputer/Class1.cs
@@ -0,0 +1,17 @@
+﻿using KRPC.Service.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kRPC_Class_for_RemoteTech_FlightComputer
+{
+    /// <summary>
+    /// The flight computer from RemoteTech
+    /// </summary>
+    [KRPCClass (Service = "RemoteTech")]
+    public class FlightComputer
+    {
+    }
+}
