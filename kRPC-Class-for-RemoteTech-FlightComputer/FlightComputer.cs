76deb919b9a72cac5c52894ee37a3696a7a000c2
Eli112358 <Eli112358.SARP@hotmail.com>
2016-09-03 23:19:04 -0400

    Added burn method

--- kRPC-Class-for-RemoteTech-FlightComputer/FlightComputer.cs
+++ kRPC-Class-for-RemoteTech-FlightComputer/FlightComputer.cs
@@ -1,9 +1,6 @@
 ﻿using KRPC.Service.Attributes;
+using KRPC.SpaceCenter.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace kRPC_Class_for_RemoteTech_FlightComputer
 {
@@ -13,6 +10,27 @@ namespace kRPC_Class_for_RemoteTech_FlightComputer
     [KRPCClass (Service = "RemoteTech")]
     public class FlightComputer
     {
+        /// <summary>
+        /// Execute a burn
+        /// </summary>
+        /// <param name="throttle"></param>
+        /// <param name="durationInSeconds"></param>
+        /// <param name="extraDelayInSeconds"></param>
+        /// <returns></returns>
+        [KRPCMethod]
+        public bool burn (double throttle, double durationInSeconds, double extraDelayInSeconds)
+        {
+            ConfigNode node = new ConfigNode("BurnCommand");
+            node.AddValue("Throttle", throttle);
+            node.AddValue("Duration", durationInSeconds);
+            node.AddValue("DeltaV", 0);
+            node.AddValue("KaCItemId", "");
+            node.AddValue("TimeStamp", SpaceCenter.UT);
+            node.AddValue("ExtraDelay", extraDelayInSeconds);
+            node.AddValue("Guid", new Guid());
+            return sendCommand(node);
+        }
+        
         /// <summary>
         /// Internal method to send a command to the flight computer
         /// </summary>
