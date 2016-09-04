8580133e113f94b04bfe3f88ac0005dbe22669ac
Eli112358 <Eli112358.SARP@hotmail.com>
2016-09-03 22:36:03 -0400

    Added internal method sendCommand

--- kRPC-Class-for-RemoteTech-FlightComputer/FlightComputer.cs
+++ kRPC-Class-for-RemoteTech-FlightComputer/FlightComputer.cs
@@ -13,5 +13,14 @@ namespace kRPC_Class_for_RemoteTech_FlightComputer
     [KRPCClass (Service = "RemoteTech")]
     public class FlightComputer
     {
+        /// <summary>
+        /// Internal method to send a command to the flight computer
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        internal bool sendCommand (ConfigNode node)
+        {
+            return RemoteTech.API.API.QueueCommandToFlightComputer(node);
+        }
     }
 }
